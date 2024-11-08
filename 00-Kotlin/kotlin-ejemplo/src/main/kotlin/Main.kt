8b1c6bf8bed620d574f97d13fb822d6049d00570
Anthony.Reinoso <pitufin26004@hotmail.com>
2024-11-08 09:01:04 -0500

    AM

---
+++ 00-Kotlin/kotlin-ejemplo/src/main/kotlin/Main.kt
@@ -0,0 +1,205 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    // INMUTABLES (No se RE ASIGNA "=")
+    val inmutable: String = "Adrian";
+    // inmutable = "Vicente" // Error!
+    // MUTABLES
+    var mutable: String = "Vicente"
+    mutable = "Adrian" // Ok
+    // VAL > VAR
+
+
+    // Duck Typing
+    val ejemploVariable = " Adrian Eguez "
+    ejemploVariable.trim()
+    val edadEjemplo: Int = 12
+    // ejemploVariable = edadEjemplo // Error!
+    // Variables Primitivas
+    val nombreProfesor: String = "Adrian Eguez"
+    val sueldo: Double = 1.2
+    val estadoCivil: Char = 'C'
+    val mayorEdad:Boolean = true
+    // Clases en Java
+    val fechaNacimiento: Date = Date()
+
+    // When (Switch)
+    val estadoCivilWhen = "C"
+    when (estadoCivilWhen){
+        ("C") ->{
+            println("Casado")
+        }
+        "S" -> {
+            println("Soltero")
+        }
+        else ->{
+            println("No sabemos")
+        }
+    }
+    val esSoltero = (estadoCivilWhen == "S")
+    val coqueteo = if (esSoltero) "Si" else "No" // if else chiquito
+
+    imprimirNombre("Adrian")
+
+
+    calcularSueldo(10.00) // solo paramtro requerido
+    calcularSueldo(10.00,15.00,20.00) // parametro requerido y sobreescribir parametros opcionales
+    // Named parameters
+    // calcularSueldo(sueldo, tasa, bonoEspecial)
+    calcularSueldo(10.00, bonoEspecial = 20.00) // usando el parametro bonoEspecial en 2da posicion
+                                                // gracias a los parametros nombrados
+    calcularSueldo(bonoEspecial = 20.00, sueldo=10.00, tasa=14.00)
+    // usando el parametro bonoEspecial en 1ra posicion
+    // usando el parametro sueldo en 2da posicion
+    // usando el parametro tasa en 3era posicion
+    // gracias a los parametros nombrados
+    // CLASES USO:
+    val sumaA = Suma(1,1)
+    val sumaB = Suma(null, 1)
+    val sumaC = Suma(1, null)
+    val sumaD = Suma(null, null)
+    sumaA.sumar()
+    sumaB.sumar()
+    sumaC.sumar()
+    sumaD.sumar()
+    println(Suma.pi)
+    println(Suma.elevarAlCuadrado(2))
+    println(Suma.historialSumas)
+}
+
+
+fun imprimirNombre(nombre:String): Unit{
+    fun otraFuncionAdentro(){
+        println("Otra funcion adentro")
+    }
+    println("Nombre: $nombre") // Uso sin llaves
+    println("Nombre: ${nombre}") // Uso con llaves opcional
+    println("Nombre: ${nombre + nombre}") // Uso con llaves (concatenado)
+    println("Nombre: ${nombre.toString()}") // uso con llaves (funcion)
+    println("Nombre: $nombre.toString()") // INCORRECTO!
+    // (no pueden usar sin llaves)
+    otraFuncionAdentro()
+}
+
+fun calcularSueldo(
+    sueldo:Double, // Requerido
+    tasa: Double = 12.00, // Opcional (defecto)
+    bonoEspecial:Double? = null // Opcional (nullable)
+    // Variable? -> "?" Es Nullable (osea que puede en algun momento ser nulo)
+):Double {
+    // Int -> Int? (nullable)
+    // String -> String? (nullable)
+    // Date -> Date? (nullable)
+    if(bonoEspecial == null){
+        return sueldo * (100/tasa)
+    }else{
+        return sueldo * (100/tasa) * bonoEspecial
+    }
+}
+
+
+abstract class NumerosJava{
+    protected val numeroUno:Int
+    private val numeroDos: Int
+    constructor(
+        uno:Int,
+        dos:Int
+    ){
+        this.numeroUno = uno
+        this.numeroDos = dos
+        println("Inicializando")
+    }
+}
+
+abstract class Numeros( // Constructor Primario
+    // Caso 1) Parametro normal
+    // uno:Int , (parametro (sin modificador acceso))
+
+    // Caso 2) Parametro y propiedad (atributo) (protected)
+    // private var uno: Int (propiedad "instancia.uno")
+    protected val numeroUno: Int, // instancia.numeroUno
+    protected val numeroDos: Int, // instancia.numeroDos
+    parametroNoUsadoNoPropiedadDeLaClase:Int? = null
+){
+    init { // bloque constructor primario OPCIONAL
+        this.numeroUno
+        this.numeroDos
+        println("Inicializando")
+    }
+}
+
+class Suma( // Constructor primario
+    unoParametro: Int, // Parametro
+    dosParametro: Int, // Parametros
+): Numeros( // Clase papa, Numeros (extendiendo)
+    unoParametro,
+    dosParametro
+){
+
+    public val soyPublicoExplicito: String = "Publicas"
+    val soyPublicoImplicite: String = "Publico implicito"
+    init { // bloque constructor primario
+        this.numeroUno
+        this.numeroDos
+        numeroUno // this. OPCIONAL  [propiedades, metodos]
+        numeroDos // this. OPCIONAL  [propiedades, metodos]
+        this.soyPublicoImplicite
+        soyPublicoExplicito
+    }
+    constructor( // Constructor secundario
+        uno: Int?, // Entero nullable
+        dos: Int,
+    ):this(
+        if(uno == null) 0 else uno,
+        dos
+    ){
+        // Bloque de codigo de constructor secundario
+    }
+    constructor( // Constructor secundario
+        uno: Int,
+        dos: Int?, // Entero nullable
+    ):this(
+        uno,
+        if(dos == null) 0 else dos
+    )
+    constructor( // Constructor secundario
+        uno: Int?, // Entero nullable
+        dos: Int?,  // Entero nullable
+    ):this(
+        if(uno == null) 0 else uno,
+        if(dos == null) 0 else dos
+    )
+    fun sumar ():Int{
+        val total = numeroUno + numeroDos
+        agregarHistorial(total)
+        return total
+    }
+    companion object { // Comparte entre todas las instancias, similar al STATIC
+        // funciones, variables
+        // NombreClase.metodo, NombreClase.funcion =>
+        // Suma.pi
+        val pi = 3.14
+        // Suma.elevarAlCuadrado
+        fun elevarAlCuadrado(num:Int):Int{ return num * num }
+        val historialSumas = arrayListOf<Int>()
+        fun agregarHistorial(valorTotalSuma:Int){ // Suma.agregarHistorial
+            historialSumas.add(valorTotalSuma)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
